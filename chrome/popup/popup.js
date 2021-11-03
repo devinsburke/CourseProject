a3164f5f88b62a081548a9f5c087ed29530a4bd7
Josh Ray <26936493+joshuaray@users.noreply.github.com>
2021-11-03 00:27:00 -0500

    Popup changes

--- chrome/popup/popup.js
+++ chrome/popup/popup.js
@@ -1,20 +1,28 @@
-let changeColor = document.getElementById("changeColor");
-
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
-
 function callback(obj) {
   console.log(obj);
   document.body.innerHTML = obj.element;
 }
 
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.tabs.sendMessage(
-    tab.id,
-    {from: 'popup', subject: 'body'},
-    callback
-  );
+document.addEventListener('DOMContentLoaded', function () {
+  chrome.tabs.query({ active: true, currentWindow: true }).then(tab => {
+    console.log(tab[0]);
+    chrome.tabs.sendMessage(
+      tab[0].id,
+      {from: 'popup', subject: 'body'},
+      callback
+    );
+  });
 });
+
+// chrome.storage.sync.get("color", ({ color }) => {
+//   changeColor.style.backgroundColor = color;
+// });
+
+// changeColor.addEventListener("click", async () => {
+//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+//   chrome.tabs.sendMessage(
+//     tab.id,
+//     {from: 'popup', subject: 'body'},
+//     callback
+//   );
+// });
