a980062b2a1ed4153ef762af7543e029c5aada19
Josh Ray <26936493+joshuaray@users.noreply.github.com>
2021-11-04 20:04:03 -0500

    Popup changes

--- chrome/popup/popup.js
+++ chrome/popup/popup.js
@@ -1,7 +1,9 @@
 function callback(obj) {
   console.log(obj);
   document.body.innerHTML = obj.element;
+  document.getElementsByTagName('terse-bg')[0].setAttribute('content-after', 'Σ');
 }
+
 
 document.addEventListener('DOMContentLoaded', function () {
   chrome.tabs.query({ active: true, currentWindow: true }).then(tab => {
