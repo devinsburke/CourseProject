feeabde446738131183c79e012b1b06b151d02a5
Josh Ray <26936493+joshuaray@users.noreply.github.com>
2021-11-06 14:28:35 -0500

    Preliminary relevance testing

--- chrome/terse/constants.js
+++ chrome/terse/constants.js
@@ -25,3 +25,16 @@ const terseAbout = [
     'Terse is an intelligent browsing extension that summarizes any text-based web page you visit. It will pull the most relevant/descriptive sentences from the page and display those as a bulleted summary. The number of sentences shown will vary depending on the length of the document and applicable settings above. Terse aims for efficient comprehension of a page with minimal time investment, by performing text retrieval using various algorithms and natural language processing techniques.',
     'Algorithms and methods utilized include utilization of the vector-space model using bag-of-words representation of each sentence a page. A similarity matrix using dot-matrix is implemented as well, and cosine similarity between each sentence is measured based on the weight of overlapping words. In order to implement the benefit of IDF, Terse utilizes a stop-word collection to minimize the impact of common words.'
 ]
+
+const terseTests = [
+    {
+        'url': '',
+        'originalText': '',
+        'userRelevance': [
+
+        ],
+        'terseRelevance': [
+            
+        ]
+    }
+]
