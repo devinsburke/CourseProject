08db9f04bd39bc78bb121a1db1d9fa66ab74b25b
Devin Burke <devinburke@gmail.com>
2021-11-06 17:36:14 -0500

    automatic whitespace adjustments

--- chrome/terse/constants.js
+++ chrome/terse/constants.js
@@ -1,25 +1,25 @@
 const terseSettings = [
     {
-      'name': 'Show Terse button on all pages', 
-      'description': 'Controls visibility of the Terse summarization tool as a floating button on all applicable pages.',
-      'type': 'boolean',
-      'defaultValue': true
+        'name': 'Show Terse button on all pages',
+        'description': 'Controls visibility of the Terse summarization tool as a floating button on all applicable pages.',
+        'type': 'boolean',
+        'defaultValue': true
     },
     {
-      'name': 'Suppress on landing pages',
-      'description': 'If set, hide the Terse button on all website home pages (i.e. hidden on \'wikipedia.org\', but shown on \'wikipedia.org/article\')',
-      'type': 'boolean',
-      'defaultValue': true
+        'name': 'Suppress on landing pages',
+        'description': 'If set, hide the Terse button on all website home pages (i.e. hidden on \'wikipedia.org\', but shown on \'wikipedia.org/article\')',
+        'type': 'boolean',
+        'defaultValue': true
     },
     {
-      'name': 'Summary size',
-      'description': 'Determines the level of verbosity of the generated page summary.',
-      'type': 'slider',
-      'min': 1,
-      'max': 3,
-      'defaultValue': 2
+        'name': 'Summary size',
+        'description': 'Determines the level of verbosity of the generated page summary.',
+        'type': 'slider',
+        'min': 1,
+        'max': 3,
+        'defaultValue': 2
     }
-  ];
+];
 
 const terseAbout = [
     'Terse is an intelligent browsing extension that summarizes any text-based web page you visit. It will pull the most relevant/descriptive sentences from the page and display those as a bulleted summary. The number of sentences shown will vary depending on the length of the document and applicable settings above. Terse aims for efficient comprehension of a page with minimal time investment, by performing text retrieval using various algorithms and natural language processing techniques.',
@@ -34,7 +34,7 @@ const terseTests = [
 
         ],
         'terseRelevance': [
-            
+
         ]
     }
 ]
