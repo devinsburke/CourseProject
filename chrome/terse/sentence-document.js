137cd70f83546dfeb93a8554830944d758e9e275
Devin Burke <devinburke@gmail.com>
2021-11-06 20:58:51 -0500

    enhanced the sentence parser to handle quotes slightly better

--- chrome/terse/sentence-document.js
+++ chrome/terse/sentence-document.js
@@ -1,5 +1,5 @@
 const defaultStopwords = ['i','me','my','myself','we','our','ours','ourselves','you','you\'re','you\'ve','you\'ll','you\'d','your','yours','yourself','yourselves','he','him','his','himself','she','she\'s','her','hers','herself','it','it\'s','its','itself','they','them','their','theirs','themselves','what','which','who','whom','this','that','that\'ll','these','those','am','is','are','was','were','be','been','being','have','has','had','having','do','does','did','doing','a','an','the','and','but','if','or','because','as','until','while','of','at','by','for','with','about','against','between','into','through','during','before','after','above','below','to','from','up','down','in','out','on','off','over','under','again','further','then','once','here','there','when','where','why','how','all','any','both','each','few','more','most','other','some','such','no','nor','not','only','own','same','so','than','too','very','s','t','can','will','just','don','don\'t','should','should\'ve','now','d','ll','m','o','re','ve','y','ain','aren','aren\'t','couldn','couldn\'t','didn','didn\'t','doesn','doesn\'t','hadn','hadn\'t','hasn','hasn\'t','haven','haven\'t','isn','isn\'t','ma','mightn','mightn\'t','mustn','mustn\'t','needn','needn\'t','shan','shan\'t','shouldn','shouldn\'t','wasn','wasn\'t','weren','weren\'t','won','won\'t','wouldn','wouldn\'t','www','com'];
-const defaultAbbreviations = ['abr','apr','aug','ave','cir','ct','dec','dr','ed','etc','feb','gen','inc','jan','jr','jul','jun','ln','mar','mr','mrs','nov','oct','prof','rep','rd','rev','sen','sep','sr','st','v','vol','vs'];
+const defaultAbbreviations = ['abr','apr','aug','ave','cir','ct','dec','dr','ed','etc','et al','feb','gen','inc','jan','jr','jul','jun','ln','mar','mr','mrs','nov','oct','pp','prof','rep','rd','rev','sen','sep','sr','st','vol','vs'];
 
 class TerseSentenceDocument {
 	constructor(original, words, vocabulary, score, sortOrder) {
@@ -20,13 +20,20 @@ class TerseSentencesDocumentProcessor {
 		this.topPercent = topPercent;
 		this.nlp = new TerseNaturalLanguageProcessor(this.stopwords);
 		this.terminators = {
-			sentence: new RegExp('(?:[\\!\\?\\r\\n]+)|(?:(?<!\\b(?:' + this.abbreviations.join('|') + '|[a-z]))\\.+(?![\\w\\.\\!\\?]))', 'gi'),
+			sentence: new RegExp('(?:[\\!\\?\\r\\n]+[\"\']?)|(?:(?<!\\b(?:' + this.abbreviations.join('|') + '|[a-z]))\\.+(?![\\w\\.\\!\\?])[\"\']?)', 'gi'),
 			word: new RegExp('(?:^\\[.*\\])|(?:[^a-z\\.\\s]+)|(?:(?<!\\b[a-z])\\.)|(?:(?<!\\b[a-z]\\.)\\s)|(?:\\s(?![a-z]\\.))', 'gi'),
 		};
 		this.documents = this.splitSentencesAsDocuments(text);
 	}
 	
 	splitSentencesAsDocuments(text) {
+		text = text
+			.replace('."', '".')
+			.replace('.\'', '\'.')
+			.replace('?"', '"?')
+			.replace('?\'', '\'?')
+			.replace('!"', '"!')
+			.replace('!\'', '\'!');
 		var docs = text.split(this.terminators.sentence).map(s => s && s.trim()).filter(n => n);
 		var lists = docs.map(d => d.toLowerCase().split(this.terminators.word).map(w => w && w.trim()).filter(w => w));
 		var bags = lists.map(s => this.nlp.toBagOfWords(s, true));
